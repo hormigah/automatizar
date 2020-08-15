7fbea6a6164c5d6057efab23f13437bb6da5242a
Wilder Villarraga <wilder.villarraga@globant.com>
2020-08-15 17:30:10 -0500

    update

---
+++ themes/custom/villadigital/js/villadigital.js
@@ -0,0 +1,32 @@
+/**
+ * @file
+ * Defines Javascript behaviors for the Motollantas Theme.
+ */
+
+(function ($, Drupal) {
+  'use strict';
+
+  Drupal.behaviors.VillaDigitalTheme = {
+    attach: function (context) {
+      $(".slick-4items .view-content", context).once('slider-4items').each(function () {
+        $(this).slick({
+          dots: true,
+          arrows: false,
+          infinite: true,
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3
+              }
+            }
+          ]
+        });
+      });
+    }
+  };
+  
+})(jQuery, Drupal);
