794ac29cd1108fb85c3cd644faee8fac24ed0840
Wilder Villarraga <hormigah@gmail.com>
2020-10-11 12:03:18 -0500

    update

---
+++ modules/custom/portal/src/Plugin/DsField/ContactLink.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\elearning\Plugin\DsField;
+
+use Drupal\ds\Plugin\DsField\DsFieldBase;
+use Drupal\Core\Link;
+
+/**
+ * Plugin that renders the course request.
+ *
+ * @DsField(
+ *   id = "contact_limk",
+ *   title = @Translation("Contact link"),
+ *   entity_type = "node",
+ *   provider = "portal",
+ *   ui_limit = {
+ *    "industry|*"
+ *   }
+ * )
+ */
+class ContactLink extends DsFieldBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $link_options = [
+      'attributes' => [
+        'class' => ['btn', 'btn-secundary'],
+      ],
+    ];
+
+    $build = Link::createFromRoute('Contacta a un asesor', 'entity.node.canonical', ['node' => 1], $link_options);
+
+    return $build;
+  }
+
+}
